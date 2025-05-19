18cb4199a9c8a75ff478eecbe19820a8a1680601
EmanuelleLavinia <0001104489@senaimgaluno.com.br>
2025-05-19 14:47:10 -0300

    Update saida6.php

--- atividade4_Emanuelle/pages/formulario_6/saida6.php
+++ atividade4_Emanuelle/pages/formulario_6/saida6.php
@@ -1,1 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $produto = htmlspecialchars($_POST['produto']);
+    $avaliacao = intval($_POST['avaliacao']);
+    $comentarios = htmlspecialchars($_POST['comentarios']);
 
+    echo "<h1>Resumo do Feedback</h1>";
+    echo "Produto: " . $produto . "<br>";
+    echo "Avaliação: " . str_repeat("★", $avaliacao) . str_repeat("☆", 5 - $avaliacao) . " ($avaliacao estrelas)<br>";
+    echo "Comentários:<br>" . nl2br($comentarios);
+} else {
+    echo "Nenhum feedback foi enviado.";
+}
+?>
